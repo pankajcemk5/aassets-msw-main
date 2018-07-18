c348ccefdcf08c9239ede6f1744f3fa89dcf806a
rishishrivastav321 <priyankaa6>
2018-07-18 12:26:18 +0530

    Rishi - updated the header

--- src/app/common-components/header/header.component.ts
+++ src/app/common-components/header/header.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrl:'./header.component.css'  
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  myFunc() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
